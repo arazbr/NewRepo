f503f4227ac57df8d76303ef85a7e14055d31783
arazbr <araz57@yahoo.com>
2025-06-05 22:52:01 +0330

    Add project files.

---
+++ VirastyarWordAddin/VirastyarWordAddin/Configurations/IAddinConfigurationDialog.cs
@@ -0,0 +1,9 @@
+﻿namespace VirastyarWordAddin.Configurations
+{
+    public interface IAddinConfigurationDialog
+    {
+        //event ShortcutChangedEventHandler ShortcutChanged;
+        event RefineAllSettingsChangedEventHandler RefineAllSettingsChanged;
+        event SpellCheckSettingsChangedEventHandler SpellCheckSettingsChanged;
+    }
+}
---
+++ VirastyarWordAddin/VirastyarWordAddin/VerificationTypes.cs
@@ -0,0 +1,18 @@
+﻿namespace VirastyarWordAddin
+{
+    public enum VerificationTypes
+    {
+        Error,
+        Warning,
+        Information
+    }
+
+    /// <summary>
+    /// Represents modes of text processing in Verifier classes.
+    /// </summary>
+    public enum TextProcessType
+    {
+        Interactive,
+        Batch
+    }
+}
